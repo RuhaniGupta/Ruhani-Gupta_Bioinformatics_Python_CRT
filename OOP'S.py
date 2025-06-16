74825887f38015a3ece0eb5a6c5e4f63c6826809
RuhaniGupta <ruhaniigupta62@gmail.com>
2025-06-16 13:54:34 +0530

    Add files via upload

---
+++ OOP'S.py
@@ -0,0 +1,12 @@
+class Student():
+    def __init__(self):
+        print("Hey...! I'm a Constructor of Student class")
+        print("I will be automatically invoked when the object is created")
+S1=Student()# __init__(self)
+S2=Student()
+S3=Student()
+S4=Student()
+S5=Student()
+
+
+    
