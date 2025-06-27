88d20723a8e9c7334696eba071da2255b05661a7
RuhaniGupta <ruhaniigupta62@gmail.com>
2025-06-27 15:00:18 +0530

    Delete Seventh.py

--- Seventh.py
+++
@@ -1,6 +0,0 @@
-#Write a python program to  print the multiplication table from 1 to n.
-Num=int(input("Enter a value of Number:"))
-for i in range(1,Num+1):
-    print(f"Multiplication table of {i}:")
-    for j in range(1,11):
-        print(f"{i}x{j}={i*j}")
