80605a0336ea01f2efbb81313cff31878098fee6
RuhaniGupta <ruhaniigupta62@gmail.com>
2025-06-27 13:58:29 +0530

    Delete Dict4.py

--- Dict4.py
+++
@@ -1,7 +0,0 @@
-def find_differences(seq1,seq2):
-    if len(seq1)!=len(seq2):
-        raise ValueError
-    difference = [i for i in range (len(seq1)) if seq1[i] != seq2[i]]
-print (difference)
-                  
-        
