f6013219db8bad020a39e586e0ee8073785ac132
RuhaniGupta <ruhaniigupta62@gmail.com>
2025-06-27 15:00:09 +0530

    Delete practice1.py

--- practice1.py
+++
@@ -1,9 +0,0 @@
-#write a python program to read a mail id as input from the user and print user name and organization name name@org.com.
-Mail_id=input("Enter the Mail id :")
-List=Mail_id.split('@')
-print(List)
-print(f"Username : {List[0]}")
-Org=List[1]
-List=Org.split('.')
-print(f"Org Name :{List[0]}")
-
