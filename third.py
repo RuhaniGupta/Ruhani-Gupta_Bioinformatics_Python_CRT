1b91cf687d94f7d74e5a46c0c79934fb6fef7078
RuhaniGupta <ruhaniigupta62@gmail.com>
2025-06-27 16:15:33 +0530

    Delete third.py

--- third.py
+++
@@ -1,6 +0,0 @@
-#Write a pyhton program to read a intput value from the user and check whether it is a two digit number or not a tw
-num = int(input("Enter a number: "))
-if (num >= 10 and num <= 99) or (num <= -10 and num >= -99):
-    print("It is a two-digit number")
-else:
-    print("It is not a two-digit number")
