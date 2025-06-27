f7492d72d0f61d45a74f5932883b33521bb2f793
RuhaniGupta <ruhaniigupta62@gmail.com>
2025-06-27 14:03:30 +0530

    Delete Function4.py

--- Function4.py
+++
@@ -1,5 +0,0 @@
-#Write a python program to build a function using which prints the multiplication table of n.
-def multiplication_table(n):
-    for i in range(1,11):
-        print(f"{n} x {i} = {n * i}")
-multiplication_table(5)
