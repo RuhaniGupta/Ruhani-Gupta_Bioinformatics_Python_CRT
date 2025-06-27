3dff332b8c873174671cb551deb9a800b5f7da57
RuhaniGupta <ruhaniigupta62@gmail.com>
2025-06-27 14:02:51 +0530

    Delete Function1.py

--- Function1.py
+++
@@ -1,9 +0,0 @@
-def display():
-    return "Hey..! I'm the Zero Arg Functionm....!"
-Res=display()
-print(Res)
-def NamedFunction(name):
-    return f"Hey..! I'm the one Arg function called by {name}"
-Res1=NamedFunction("kavya")
-print(Res1)
-
