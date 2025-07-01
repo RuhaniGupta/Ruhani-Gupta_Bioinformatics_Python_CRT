2639a5864f7a37db39d897b563cc950f9253ae2b
RuhaniGupta <ruhaniigupta62@gmail.com>
2025-07-01 09:26:02 +0530

    Add files via upload

---
+++ Backtracking.py
@@ -0,0 +1,18 @@
+'''A backtracking algorithm is a problem-solving algorithm that uses a brute force approac for finding the desaired output'''
+
+'''A brute force approach tries out all the possible solutions and chooses the  best solutions'''
+
+'''The term backtracking suggests that if the current solution is not suitable,then backtrack and try other solutions.Thus,recursion is used in
+this approach'''
+
+'''Types of problems
+
+Decision Problem : In this, we search for a feasible solution.
+
+Optimization Problem : In this, we search for the best solution.
+
+Enumeration Problem : In this, we find all feasible solutions.'''
+
+
+'''In recursion, the function call itself until it reaches a base cas. In backtracking, we recursion to explore all the possibilities until we 
+get the get result for the problem.'''
